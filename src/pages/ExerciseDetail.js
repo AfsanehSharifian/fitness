5c93205b315ba34bf7c21d6474ead95f6a34c658
Afsaneh Sharifian <afsaneh.sharifian2k@gmail.com>
2025-02-18 21:07:14 +0330

    passing the exercise data to the exerciseData component

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect, use } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
 
 const ExerciseDetail = () => {
-  return (
-    <div>ExerciseDetail</div>
-  )
-}
+  const [exersiceDetail, setExersiceDetail] = useState({});
 
-export default ExerciseDetail
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/${id}`,
+        exerciseOptions
+      );
+      setExersiceDetail(exerciseDetailData);
+    };
+
+    fetchExercisesData();
+  }, [id]);
+
+  return (
+    <Box>
+      <Detail exerciseDetail={exersiceDetail} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
+  );
+};
+
+export default ExerciseDetail;
